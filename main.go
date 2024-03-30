d344ceebfaf9e0b32fe4ab6d6810fabf7a61014f
samiam2013 <samiam2013@gmail.com>
2024-03-30 12:52:58 -0500

    use a channel to allow requests to run in the background, allowing responses to seem immediate

--- main.go
+++ main.go
@@ -24,12 +24,12 @@ func main() {
 		log.Fatalf("ANTHROPIC_API_KEY env var is not set")
 	}
 
-	voice.Say("welcome to crossfire")
+	voice.Say("welcome to crossfire", voice.WithVoice(voice.Daniel))
 	oAPI := openai.NewAPI(oAPIkey)
 	anthAPI := anthropic.NewAPI(anthAPIkey)
 
-	debateTopic := "determinism is true rather than free will (whether or not it subjectively matters)" // simulation theory suggested by copilot
-	voice.Say("The topic of today's debate is: " + debateTopic)
+	debateTopic := "multi-level polymorphic inheritance is bad in software code" // simulation theory suggested by copilot
+	voice.Say("The topic of today's debate is: "+debateTopic, voice.WithVoice(voice.Karen))
 
 	hist := history.NewMessageHistory()
 	anthResp, err := anthAPI.GetMessageResponse("Make your best case for '"+debateTopic+"' is true", hist)
@@ -40,9 +40,14 @@ func main() {
 	if err != nil {
 		log.WithError(err).Fatalf("failed to get first content from first message")
 	}
-	voice.Say("The first argument by claude for '" + debateTopic + "' being true: " + contentStr)
-
 	hist.Add(history.AuthorClaude, contentStr)
+
+	talkingStick := make(chan struct{})
+	go func(ts chan struct{}) {
+		voice.Say("The first argument by claude for '"+debateTopic+"' being true: "+contentStr, voice.WithVoice(voice.Karen))
+		ts <- struct{}{}
+	}(talkingStick)
+
 	for {
 		oAPIPrompt := "Make your best case for why the following argument for '" +
 			debateTopic + "' being false in response to this argument: " + contentStr
@@ -54,8 +59,12 @@ func main() {
 		if err != nil {
 			log.WithError(err).Fatalf("failed to get first content from openai response")
 		}
-		voice.Say("The response argument by openai: "+oAPIFirstContent, 1)
 		hist.Add(history.AuthorOpenAI, oAPIFirstContent)
+		<-talkingStick
+		go func(ts chan struct{}) {
+			voice.Say("The response argument by open ai: "+oAPIFirstContent, voice.WithVoice(voice.Daniel))
+			ts <- struct{}{}
+		}(talkingStick)
 
 		// now get the response from anthropic and put in contentStr
 		anthResp, err = anthAPI.GetMessageResponse("Make your best case for why the following argument for '"+
@@ -67,7 +76,13 @@ func main() {
 		if err != nil {
 			log.WithError(err).Fatalf("failed to get first content from first message")
 		}
-		voice.Say("The response argument by claude: " + contentStr)
 		hist.Add(history.AuthorClaude, contentStr)
+
+		<-talkingStick
+		go func(ts chan struct{}) {
+			voice.Say("The response argument by claude: "+contentStr, voice.WithVoice(voice.Karen))
+			ts <- struct{}{}
+		}(talkingStick)
+
 	}
 }
--- pkg/voice/say.go
+++ pkg/voice/say.go
@@ -7,26 +7,41 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// say either uses exec to say the text or fatally logs the error
-func Say(text string, voiceSetting ...int) {
-	fmt.Println(text + "\n\n")
-	var voice string
-	if len(voiceSetting) == 0 {
-		voiceSetting = []int{0}
-	}
-	if len(voiceSetting) != 1 {
-		panic("invalid number of voice settings (should be one or none)")
-	}
-	switch voiceSetting[0] {
-	case 0:
-		voice = "Karen"
-	case 1:
-		voice = "Daniel"
-	default:
-		panic("invalid voice")
-	}
+type VoiceChoice string
 
-	command := exec.Command("say", "-v", voice, fmt.Sprintf("\"[[volm 0.1]] %s\"", text))
+const (
+	Karen  VoiceChoice = "Karen"
+	Daniel VoiceChoice = "Daniel"
+)
+
+type VoiceConfig struct {
+	voice  VoiceChoice
+	volume float64
+}
+
+type Option func(*VoiceConfig)
+
+func WithVoice(v VoiceChoice) Option {
+	return func(vc *VoiceConfig) {
+		vc.voice = v
+	}
+}
+
+// say either uses exec to say the text or fatally logs the error
+func Say(text string, opts ...Option) {
+	vc := &VoiceConfig{}
+	for _, opt := range opts {
+		opt(vc)
+	}
+	if vc.voice == "" {
+		vc.voice = Karen
+	}
+	if vc.volume == 0.0 || vc.volume > 1.0 || vc.volume < 0.0 {
+		vc.volume = 1.0
+	}
+	fmt.Println(text + "\n\n")
+
+	command := exec.Command("say", "-v", string(vc.voice), fmt.Sprintf("\"[[volm %.2f]] %s\"", vc.volume, text))
 	if err := command.Run(); err != nil {
 		log.WithError(err).Fatalf("failed to run say command")
 	}
